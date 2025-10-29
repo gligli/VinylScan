d48d44fc055ac40e4212a1a226d558c382d815f2
GliGli <sfx.gligli@free.fr>
2025-10-29 12:36:02 +0100

    alglib-cpp-vinylscan: cleanup, added alglib_BoxConstrainedMinimize

--- alglib-cpp/dll.cpp
+++ alglib-cpp/dll.cpp
@@ -43,7 +43,9 @@ extern "C"
     minnscreate(x, state);
     minnssetalgoags(state, Radius, Penalty);
     if (LowBound && UpBound)
+    {
       minnssetbc(state, lb, ub);
+    }
     minnssetcond(state, Epsilon, 0);
     minnssetxrep(state, true);
     minnsoptimize(state, ns_jac, ns_frep, Data);
@@ -103,14 +105,63 @@ extern "C"
     return lbfgs_f;
   }
 
-  __declspec(dllexport) double alglib_SpearmanRankCorrelation(double* X, double* Y, int n)
+  __declspec(thread) void  (*bc_cb)(int n, double* x, double* func, double* grad, void* ptr);
+  __declspec(thread) double bc_f;
+
+  void bc_grad(const real_1d_array& x, double& func, real_1d_array& grad, void* ptr)
   {
+    bc_cb(x.length(), x.c_ptr()->ptr.p_double, &func, grad.c_ptr()->ptr.p_double, ptr);
+  };
+
+  void bc_frep(const real_1d_array& x, double func, void* ptr)
+  {
+    bc_f = func;
+  };
+
+  __declspec(dllexport) double alglib_BoxConstrainedMinimize(void* Func, int n, double* X, double* LowBound, double* UpBound, double* Scale, double Epsilon, void* Data)
+  {
+    minbcstate state;
+    minbcreport rep;
     real_1d_array x;
-    real_1d_array y;
+    real_1d_array lb;
+    real_1d_array ub;
+    real_1d_array scl;
 
+    bc_cb = (void  (*)(int n, double* x, double* func, double* grad, void* ptr)) Func;
+    bc_f = fp_nan;
     x.attach_to_ptr(n, X);
-    y.attach_to_ptr(n, Y);
 
-    return spearmancorr2(x, y, n);
+    if (LowBound && UpBound)
+    {
+      lb.attach_to_ptr(n, LowBound);
+      ub.attach_to_ptr(n, UpBound);
+    }
+
+    if (Scale)
+    {
+      scl.attach_to_ptr(n, Scale);
+    }
+
+    minbccreate(n, x, state);
+    if (LowBound && UpBound)
+    {
+      minbcsetbc(state, lb, ub);
+    }
+    if (Scale)
+    {
+      minbcsetscale(state, scl);
+      minbcsetprecscale(state);
+    }
+    minbcsetcond(state, 0.0, 0.0, Epsilon, 0);
+    minbcsetxrep(state, true);
+    minbcoptimize(state, bc_grad, bc_frep, Data);
+    minbcresults(state, x, rep);
+
+    bc_cb = NULL;
+    for (int i = 0; i < n; ++i)
+      X[i] = x[i];
+
+    return bc_f;
   }
+
 }
